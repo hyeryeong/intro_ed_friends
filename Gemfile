2eb7519a89e7f35c728803dfa5d41d51feca8496
hyeryeong <hyeryeong.yoon@headflow.net>
2011-02-21 11:55:31 +0900

    Finished first cut of the User model

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 # end
 group :development do
   gem 'rspec-rails', '2.3.0'
+  gem 'annotate-models', '1.0.4'
 end
 
 group :test do
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe UsersController do
-  render_views
+  render_view
 
   describe "GET 'new'" do
     it "should be successful" do
       get 'new'
       response.should be_success
     end
-
+    
     it "should have the right title" do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
